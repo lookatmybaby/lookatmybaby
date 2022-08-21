1c78974a167eff16994dd0c97457ef3a6b95a24b
Seenee-Shin <heeyeon0705@gmail.com>
2022-08-21 17:06:57 +0900

    [Chore] 초기설정세팅

--- src/main/java/com/toyproject/lookatmybaby/LookatmybabyApplication.java
+++ src/main/java/com/toyproject/lookatmybaby/LookatmybabyApplication.java
@@ -2,8 +2,17 @@ package com.toyproject.lookatmybaby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+//public class LookatmybabyApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(LookatmybabyApplication.class, args);
+//	}
+//
+//}
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class LookatmybabyApplication {
 
 	public static void main(String[] args) {
