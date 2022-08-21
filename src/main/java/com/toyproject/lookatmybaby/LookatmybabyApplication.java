8b108b82a1f5da906a408a217d59c297a631b1d1
Seenee-Shin <heeyeon0705@gmail.com>
2022-08-21 16:09:54 +0900

    [Chore] 초기환경셋칭

---
+++ src/main/java/com/toyproject/lookatmybaby/LookatmybabyApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.lookatmybaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookatmybabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookatmybabyApplication.class, args);
+	}
+
+}
