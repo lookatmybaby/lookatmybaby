3c064a540da27e3db93502e8badc629b1ba6408f
joodb <joodb0126@gmail.com>
2022-08-26 10:34:56 +0900

    [Element] button 엘리먼트 구현
    카테고리, 작은 카테고리, 저장 버튼 UI 구현
    
     #5

--- src/main/frontend/src/elements/Button.tsx
+++ src/main/frontend/src/elements/Button.tsx
@@ -1,7 +1,46 @@
 import * as React from "react";
-import {} from './buttonStyle';
+import { CtDisabled, Btn, CtEnabled, SaveDisabled } from "./buttonStyle";
 
-const Button = () => {
-    return null;
+interface BtnProps {
+  onClick?: () => void;
+  children?: React.ReactNode;
 }
-export default { Button };
+
+export const CtEnabledBtn = ({ onClick, children }: BtnProps) => {
+	return (
+		<CtEnabled 
+			width={60}
+			fontSize={14}
+			onClick={onClick}
+		>{children}</CtEnabled>
+	)
+}
+
+export const CtDisabledBtn = ({ onClick, children }: BtnProps) => {
+	return (
+		<CtDisabled
+			width={60}
+			fontSize={14}
+			onClick={onClick} 
+		>{children}</CtDisabled>
+	)
+}
+	
+export const SmallCategory = ({ onClick, children }: BtnProps) => {
+	return (
+		<Btn 
+			width={50}
+			fontSize={10}
+		>{children}</Btn>
+	)
+}
+
+export const SaveDisabledBtn = ({ onClick, children}: BtnProps) => {
+	return (
+		<SaveDisabled
+			width={90}
+			fontSize={14}
+			onClick={onClick} 
+		>저장</SaveDisabled>
+	)
+}
--- src/main/frontend/src/elements/buttonStyle.ts
+++ src/main/frontend/src/elements/buttonStyle.ts
@@ -1,7 +1,43 @@
 import styled from "styled-components";
 
-const TagColorBtn = styled.button`
 
+export const Btn = styled.button<{ 
+	width?: number; 
+	fontSize?: number;
+	
+	}>`
+	width: ${props => props.width}px;
+	font-size: ${props => props.fontSize}px;
+  font-weight: 500;
+	padding: 4px 0;
+	color: #fff;
+	background-color: #f4800b;
+	border: 1px solid #f4800b;
+  border-radius: 5px;
+  cursor: pointer;
+	box-sizing: border-box;
+	
+	font-family: Pretendard;
 `;
 
-export default { TagColorBtn };
+export const CtEnabled = styled(Btn)`
+	&:hover {
+		background: #e77707;
+		border: 1px solid #e77707;
+	}
+`;
+
+export const CtDisabled = styled(Btn)`
+	color: #f4800b;
+	background-color: #fff;
+	
+	&:hover {
+		background: #ffebd7;
+	}
+	`;
+
+export const SaveDisabled = styled(Btn)`
+	background-color: #ffc387;
+	padding: 8px 0;
+	border: 0px;
+`;
